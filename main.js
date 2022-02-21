49dca6220c9b4c53d5379aa3aba0de08711d05c1
Margo250403 <margoshakac@gmail.com>
2022-02-21 08:44:37 +0200

    add changes

--- main.js
+++ main.js
@@ -2,9 +2,9 @@ function sort(arr) {
      for (var i = 0; i < arr.length; i++) {
           for (var j = i; j < arr.length; j++) {
                if (arr[i] < arr[j]) {
-                    var p = arr[i];
+                    var temp = arr[i];
                     arr[i] = arr[j];
-                    arr[j] = p;
+                    arr[j] = temp;
                }
           }
      }
